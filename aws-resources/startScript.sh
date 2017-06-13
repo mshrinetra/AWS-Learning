83a4c1eb0548ea0cc71151d9633ed4658a3b73e5
mshrinetra <mshrinetra@gmail.com>
2017-06-14 02:04:04 +0530

    startScript.sh corrected

--- aws-resources/startScript.sh
+++ aws-resources/startScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum update -y
 yum install httpd php git -y
-cd /var/www/http
+cd /var/www/html
 git clone https://github.com/mshrinetra/AWS-Learning.git .
 service httpd start
 chkconfig httpd on
