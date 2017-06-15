d447a8891744d6819810652df3ab221e5eded6aa
mshrinetra <mshrinetra@gmail.com>
2017-06-15 14:42:23 +0530

    heartbeet added

---
+++ heartbeet.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Hi I'm Fine.</h1> <br/>";
+echo "<h2><a href=\"index.php\">Back Home</a></h2><br/>";
+echo "<h3><a href=\"phpinfo.php\">Get Info</a></h3>";
+?>
---
+++ phpinfo.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1><a href=\"index.php\">Back Home</a></h1><br/>";
+print_r(phpinfo());
+?>
